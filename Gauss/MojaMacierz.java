9ae6867cbd097062588438e5f5399eb09721706e
akonstantynowicz <konsiax@gmail.com>
2019-11-06 16:47:19 +0100

    gauss podstawowy

--- Gauss/MojaMacierz.java
+++ Gauss/MojaMacierz.java
@@ -7,6 +7,36 @@ import java.util.Random;
 public class MojaMacierz {
     static int N = 10;
 
+    static void G(float[][] A, float[] B) {
+        int p;
+        int j;
+        float wspolczynnik;
+        for (int i = 0; i < N; i++) {
+            j = i;
+            for (int k = i + 1; k < N; k++) {
+                wspolczynnik = A[k][j] / A[i][j];
+                for (int l = j; l < N; l++) {
+                    float tmp = A[k][l];
+                    A[k][l] = tmp - (wspolczynnik * A[i][l]);
+                }
+                B[k] = B[k] - (wspolczynnik * B[i]);
+            }
+        }
+    }
+
+    static void G(double[][] A, double[] B) {
+        int j;
+        double wspolczynnik;
+            for (int k = i + 1; k < N; k++) {
+                wspolczynnik = A[k][j] / A[i][j];
+                for (int l = j; l < N; l++) {
+                    double tmp = A[k][l];
+                    A[k][l] = tmp - (wspolczynnik * A[i][l]);
+                }
+                B[k] = B[k] - (wspolczynnik * B[i]);
+            }
+        }
+
     static void PG(float[][] A, float[] B) {
         float max;
         int p;
